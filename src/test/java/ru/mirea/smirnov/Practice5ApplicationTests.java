1fae1242ae7a39ea207586a5e8532ce2ca11b21c
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-26 22:28:19 +0300

    init project

---
+++ src/test/java/ru/mirea/smirnov/Practice5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.smirnov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
